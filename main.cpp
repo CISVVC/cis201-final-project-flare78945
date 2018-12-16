011f515b3d638298f1c244553a74d6d3693b7e02
flare78945 <phillip_tran_98@yahoo.com>
2018-12-16 01:09:42 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,34 +20,60 @@ int main()
     cout << "Error in opening file.";
     return 0;
   }
-  else
-  {
-    Counter* W1 = NULL;
-    vector<Counter> words;
-    int linenumber = 1;
-    string word;
-    char ch;
-    while (in_file)
-    {
-      if (in_file.peek() <= 'Z' && in_file.peek() >= 'A')
-      {
-        ch = in_file.get();
-        tolower(ch);
-        word += ch;
-      }
-      if (in_file.peek() <= 'z' && in_file.peek() >= 'a')
-      {
-        ch = in_file.get();
-        word += ch;
-      }
-      else
-      {
-        ch = in_file.get();
-      }
 
-      W1 = new Counter(word, linenumber);
+  Counter* W1 = NULL;
+  vector<Counter> words;
+  int linenumber = 1;
+  string word;
+  char ch;
+  bool newword = false;
+  bool newline = false;
+  
+  while (in_file)
+  {
+    while (in_file.peek() <= 'Z' && in_file.peek() >= 'A')
+    {
+      ch = in_file.get();
+      tolower(ch);
+      word += ch;
     }
+    while (in_file.peek() <= 'z' && in_file.peek() >= 'a')
+    {
+      ch = in_file.get();
+      word += ch;
+    }
+    while (in_file.peek() == '\n')
+    {
+      ch = in_file.get();
+      newline = true;
+    }
+    if ((in_file.peek() < 'A' && in_file.peek() > 'Z') || (in_file.peek() < 'a' && in_file.peek() > 'z') && in_file.peek() != '\n')
+    {
+      ch = in_file.get();
+    }
+
+    for (int i = 0; i < words.size(); i++)
+    {
+      if (words[i].Same(word))
+      {
+        words[i].Increase();
+        words[i].Linetrack(linenumber);
+      }
+      else newword = true;
+    }
+    if (newword)
+    {
+      W1 = new Counter(word, linenumber);
+      words.push_back(*W1);
+      newword = false;
+    }
+    if (newline) {linenumber++;}
   }
+  delete[] W1;
   in_file.close();
+  for (int i = 0; i < words.size(); i++)
+  {
+    words[i].Print();
+  }
   return 0;
 }
